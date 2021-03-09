38c9d8de7adc8e4054017f8375e3d7d27138edda
GodGc <zuitianlai@vip.qq.com>
2021-03-10 01:10:32 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    base: "/library",
+    title: "Code-library",
+    description: "FED, 前端, React, Vue, 小程序, javascript, CSS, HTML",
+    themeConfig: {
+        logo: "http://www.godrry.com/usr/uploads/2020/01/1224649180.png",
+        nav: [
+            { text: "Library Home", link: "/" },
+            { text: "Blog Home", link: "http://www.godrry.com/" },
+        ],
+        sidebar: [
+            '/',
+            '/JavaScript/',
+            '/JavaScript/demo',
+        ],
+        displayAllHeaders: true,
+    },
+};
